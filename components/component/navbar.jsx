b7c6192f497167561ccd1a35a2a76b5060c09e57
ShinzDev <shinaayomibamgboye@gmail.com>
2024-06-11 20:48:03 +0100

    button added

--- components/component/navbar.jsx
+++ components/component/navbar.jsx
@@ -174,7 +174,7 @@ export function Navbar() {
            
             </div>
             </div>
-              <button className=" grid gap-4">
+              <button className="  grid gap-3  rounded  p-2 bg-gray-800">
             view more
           </button> 
           </div>

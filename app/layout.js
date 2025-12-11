bf734ebca07b353b2d72a4e4a85de4ed7c2643dd
Shinaayomi Bamgboye~ <shinaayomibamgboye@gmail.com>
2025-12-11 22:49:12 +0100

    clean up done

--- app/layout.js
+++ app/layout.js
@@ -1,10 +1,14 @@
 
 
 import "./globals.css";
-import { Inter } from "next/font/google";
+import { Inter, Poppins } from "next/font/google";
 
 const inter = Inter({
   subsets: ["latin"],
+});
+const poppins = Poppins({
+  subsets: ["latin"],
+    weight: ["400", "500", "600"],
 });
 
 export const metadata = {
@@ -14,7 +18,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={poppins.className}>{children}</body>
     </html>
   );
 }
--- components/projects.jsx
+++ components/projects.jsx
@@ -10,18 +10,24 @@ export default function FeaturedProjects() {
       img: "showcase.png",
       link: "https://shin-xdecour.vercel.app/",
       desc: "An events decoration website ",
+      stack: "Stack : React + Next.js",
+      github: "https://github.com/ShinzDev/bosunDecourations",
     },
     {
       title: "Automate",
       img: "auto1.png",
       link: "https://doordash-nine.vercel.app/",
-      desc: "React + Next.js app that scrapes creators' links for promo codes.",
+      desc: "Allows users to browse, book, and manage car rentals with an intuitive UI.",
+      stack: "Stack : Next.js - Tailwind CSS - Django backend",
+      github: "https://github.com/ShinzDev/Doordash",
     },
     {
       title: "Doughflow",
       img: "dough.png",
       link: "https://doughflow-ivory.vercel.app/",
       desc: "used to tracks and calculate daily sales of different bread types for a real business operation",
+      stack: "Stack : React + Next.js",
+      github: "https://github.com/ShinzDev/doughflow",
     },
   ];
 
@@ -52,18 +58,22 @@ export default function FeaturedProjects() {
                 />
               </div>
               <div className="p-2 text-left">
+                <h5> {p.stack}</h5>
+                <br />
                 {p.desc}
               </div>
-              <div className="p-5 grid grid-cols-2">
-                <button 
-                  className=""
-                  onClick={() => window.open(p.link, "_blank")}>
+              <div className="p-5 grid gap-2 grid-cols-2">
+                <button
+                  className="rounded-md p-3 hover:bg-[#BFFFD1] duration-1000  transition-all ease-in-out  font-sans font-semibold bg-white text-black"
+                  onClick={() => window.open(p.link, "_blank")}
+                >
                   check project
                 </button>
-                <button 
-                  className=""
-                  onClick={() => window.open(p.link, "_blank")}>
-                    Github
+                <button
+                  className="bg-[#08872B]  hover:bg-[#C06EFF]  rounded-md"
+                  onClick={() => window.open(p.github, "_blank")}
+                >
+                  Github
                 </button>
               </div>
             </motion.div>
